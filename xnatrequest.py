28c69f609f7cf3c63fcc1da769fac7b77af6e02b
Mrigank Maharana <133823008+mkorkrish@users.noreply.github.com>
2023-09-25 18:44:06 -0500

    Update xnatrequest.py

--- xnatrequest.py
+++ xnatrequest.py
@@ -3,6 +3,11 @@ import time
 import requests
 from concurrent.futures import ThreadPoolExecutor
 
+VALID_FILES = [
+    "bold", "T1w", "T2w", "dwi", "minIP", "swi", "T2star", "angio",
+    "asl", "fieldmap", "FLAIR", "GRE"
+]
+
 class MRIFileHandler:
     """Class to handle MRI file download, storage, and cleanup."""
     
@@ -27,26 +32,28 @@ class MRIFileHandler:
                 if i < self.RETRIES - 1:
                     time.sleep(self.DELAY)
                     continue
-                raise e
+                else:
+                    print(f"Error with URL {url}: {e}")
+                    return None
 
     def download_file(self, url, output_path):
         """Download file from URL and save to specified path."""
         if os.path.exists(output_path):
             print(f"File {output_path} already exists. Skipping download.")
             return
-        print(f"Downloading: {url}")
         response = self.robust_request(url)
-        with open(output_path, 'wb') as out_file:
-            for chunk in response.iter_content(chunk_size=self.CHUNK_SIZE):
-                out_file.write(chunk)
-        print(f"Downloaded to: {output_path}")
+        if response:
+            with open(output_path, 'wb') as out_file:
+                for chunk in response.iter_content(chunk_size=self.CHUNK_SIZE):
+                    out_file.write(chunk)
+            print(f"Downloaded to: {output_path}")
 
-    def cleanup_files(self, directory, valid_extensions):
+    def cleanup_files(self, directory):
         """Remove files not matching specific MRI types in directory."""
         print(f"Cleaning up directory: {directory}")
         for root, _, files in os.walk(directory):
             for file in files:
-                if not file.endswith(valid_extensions):
+                if not any(valid_file in file for valid_file in VALID_FILES):
                     file_path = os.path.join(root, file)
                     os.remove(file_path)
                     print(f"Removed: {file_path}")
@@ -55,13 +62,14 @@ class MRIFileHandler:
         """Retrieve the subject and experiment ID for a specific MR ID."""
         url = f"{self.BASE_URL}/experiments?xsiType=xnat:imageSessionData&columns=ID,project,subject_ID,label&label={mr_id}"
         response = self.robust_request(url)
-        try:
-            experiments = response.json()['ResultSet']['Result']
-        except Exception as e:
-            print(f"Error parsing JSON for MR ID {mr_id}. Response content: {response.text}")
-            return None, None
-        if experiments:
-            return experiments[0]['subject_ID'], experiments[0]['ID']
+        if response:
+            try:
+                experiments = response.json()['ResultSet']['Result']
+            except Exception as e:
+                print(f"Error parsing JSON for MR ID {mr_id}. Response content: {response.text}")
+                return None, None
+            if experiments:
+                return experiments[0]['subject_ID'], experiments[0]['ID']
         return None, None
 
     def download_files_in_parallel(self, download_list):
@@ -73,64 +81,72 @@ class MRIFileHandler:
 if __name__ == "__main__":
     # CONSTANTS
     AUTH = ('mkorkrish', 'vujpex-fupsut-1tajJe')
-    VALID_EXTENSIONS = (
-        "T1w.nii", "T1w.nii.gz", "T2w.nii", "T2w.nii.gz", "FLAIR.nii", "FLAIR.nii.gz", 
-        "bold.nii", "bold.nii.gz", "dwi.nii", "dwi.nii.gz", "swi.nii", "swi.nii.gz", 
-        "asl.nii", "asl.nii.gz", "minIP.nii", "minIP.nii.gz", "T2star.nii", "T2star.nii.gz", 
-        "angio.nii", "angio.nii.gz", "fieldmap.nii", "fieldmap.nii.gz", "GRE.nii", "GRE.nii.gz"
-    )
 
     handler = MRIFileHandler(auth=AUTH)
 
     # Initial cleanup
     print("Starting initial cleanup...")
-    handler.cleanup_files("Training", VALID_EXTENSIONS)
-    handler.cleanup_files("Testing", VALID_EXTENSIONS)
+    handler.cleanup_files("Training")
+    handler.cleanup_files("Testing")
     print("Initial cleanup completed.")
 
     # Load MR IDs
     print("Loading MR IDs...")
-    with open(r"J:\Lists\Sorted_MR_IDs_Ages_Train_Test.txt", "r") as file:
+    with open(r"Lists\Sorted_MR_IDs_Ages_Train_Test.txt", "r") as file:
         lines = file.readlines()
 
-    mr_ids = [line.split(',')[0].strip() for line in lines if not line.startswith(("Training Data", "Testing Data", "\n"))]
+    data_type = None
     mr_id_to_info = {}
     for line in lines:
         stripped_line = line.strip()
-        if stripped_line.startswith(("Training Data", "Testing Data")):
-            data_type = stripped_line.replace(" Data:", "")
+        if "Training Data:" in stripped_line:
+            data_type = "Training"
+        elif "Testing Data:" in stripped_line:
+            data_type = "Testing"
         elif stripped_line and "," in stripped_line:
             values = stripped_line.split(',')
             mr_id = values[0].strip()
             age = values[1].strip()
             mr_id_to_info[mr_id] = {"type": data_type, "age": age}
-    print(f"Loaded {len(mr_ids)} MR IDs.")
+
+    print(f"Loaded {len(mr_id_to_info)} MR IDs.")
 
     # Process MR IDs
     download_list = []
-    for mr_id in mr_ids:
+    for mr_id, info in mr_id_to_info.items():
         print(f"Processing MR ID: {mr_id}")
         subject_id, experiment_id = handler.get_subject_experiment_for_mr_id(mr_id)
         if subject_id and experiment_id:
             url = f"{handler.BASE_URL}/projects/OASIS3/subjects/{subject_id}/experiments/{experiment_id}/scans"
             scans_response = handler.robust_request(url)
-            scans = scans_response.json()['ResultSet']['Result']
-            for scan in scans:
-                scan_label = scan['ID']
-                files_url = f"{handler.BASE_URL}/projects/OASIS3/subjects/{subject_id}/experiments/{experiment_id}/scans/{scan_label}/files"
-                files_response = handler.robust_request(files_url)
-                files = files_response.json()['ResultSet']['Result']
-                for file in files:
-                    file_name = file['Name']
-                    if file_name.endswith(('.nii', '.nii.gz')):
-                        file_url = f"{handler.BASE_URL}/projects/OASIS3/subjects/{subject_id}/experiments/{experiment_id}/scans/{scan_label}/resources/{file['collection']}/files/{file_name}"
-                        base_path = "Training" if mr_id_to_info[mr_id]["type"] == "Training" else "Testing"
-                        age_folder = os.path.join(base_path, mr_id_to_info[mr_id]["age"].replace("Age: ", "Age_"))
-                        if not os.path.exists(age_folder):
-                            os.makedirs(age_folder)
-                        output_path = os.path.join(age_folder, file_name)
-                        download_list.append((file_url, output_path))
-                handler.cleanup_files(age_folder, VALID_EXTENSIONS)
+            if scans_response:
+                try:
+                    scans = scans_response.json()['ResultSet']['Result']
+                except Exception as e:
+                    print(f"Error parsing scans JSON for MR ID {mr_id}. Response content: {scans_response.text}")
+                    continue
+
+                for scan in scans:
+                    scan_label = scan['ID']
+                    files_url = f"{handler.BASE_URL}/projects/OASIS3/subjects/{subject_id}/experiments/{experiment_id}/scans/{scan_label}/files"
+                    files_response = handler.robust_request(files_url)
+                    if files_response:
+                        try:
+                            files = files_response.json()['ResultSet']['Result']
+                        except Exception as e:
+                            print(f"Error parsing files JSON for MR ID {mr_id}, Scan {scan_label}. Response content: {files_response.text}")
+                            continue
+
+                        for file in files:
+                            file_name = file['Name']
+                            if any(valid_file in file_name for valid_file in VALID_FILES):
+                                file_url = f"{handler.BASE_URL}/projects/OASIS3/subjects/{subject_id}/experiments/{experiment_id}/scans/{scan_label}/resources/{file['collection']}/files/{file_name}"
+                                base_path = info["type"]
+                                age_folder = os.path.join(base_path, info["age"].replace("Age: ", "Age_"))
+                                if not os.path.exists(age_folder):
+                                    os.makedirs(age_folder)
+                                output_path = os.path.join(age_folder, file_name)
+                                download_list.append((file_url, output_path))
 
     print("Processing completed. Starting parallel downloads...")
     handler.download_files_in_parallel(download_list)
